166d97825f3112ffb5f933a86cbfbbb40e1bd1f2
shadow20254 <bodekarmohit@gmail.com>
2025-05-29 16:38:12 +0530

    Add files via upload

---
+++ Basic Mathematical Operations.py
@@ -0,0 +1,7 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+c=a+b
+print("addition = ",c)
+print("subtraction = ",a-b)
+print("multiplication = ",a*b)
+print("division = ",a/b)
---
+++ simple program.py
@@ -0,0 +1,9 @@
+#si unit formula = PNR/100
+p=input("Enter a number: ")
+n=input("Enter a number: ")
+r=input("Enter a number: ")
+p=int(p)
+n=int(n)
+r=int(r)
+si=(p*n*r)/100
+print(si)
